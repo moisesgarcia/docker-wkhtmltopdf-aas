2caf48bc5111bc360e5357b38c3fee90ee2db569
Moises Garcia <moises@moisesgarcia.es>
2017-05-15 13:21:34 +0200

    Now it correctly delete the tmp files generated

--- app.py
+++ app.py
@@ -7,6 +7,7 @@
 """
 import json
 import tempfile
+import os
 
 from werkzeug.wsgi import wrap_file
 from werkzeug.wrappers import Request, Response
@@ -32,17 +33,27 @@ def application(request):
             # If a JSON payload is there, all data is in the payload
             payload = json.loads(request.data)
             source_file.write(payload['contents'].decode('base64'))
+            # Load a list of files to do the merge
+            pdf_files = payload.get('fichs', {})
             options = payload.get('options', {})
+            # Load command exec file wkhtmltopdf or pdftk
+            cmd = payload['cmd']
         elif request.files:
             # First check if any files were uploaded
             source_file.write(request.files['file'].read())
+            # Load a list of files to do the merge
+            pdf_files = request.files.getlist('fichs')
             # Load any options that may have been provided in options
             options = json.loads(request.form.get('options', '{}'))
+            # Load command exec file wkhtmltopdf or pdftk
+            cmd = request.files['cmd']
 
         source_file.flush()
 
         # Evaluate argument to run with subprocess
         args = ['wkhtmltopdf']
+        if (cmd == 'pdftk'):
+            args = ['pdftk']
 
         # Add Global Options
         if options:
@@ -51,6 +62,15 @@ def application(request):
                 if value:
                     args.append('"%s"' % value)
 
+        if pdf_files and (cmd == 'pdftk') :
+            if request_is_json:
+                for f, value in pdf_files.items():
+                    sf = tempfile.NamedTemporaryFile(suffix='.pdf', delete=False)
+                    sf.write(value.decode('base64'))
+                    sf.close()
+                    # Add source file name
+                    args += [sf.name]
+
         # Add source file name and output file name
         file_name = source_file.name
         args += [file_name, file_name + ".pdf"]
@@ -58,8 +78,16 @@ def application(request):
         # Execute the command using executor
         execute(' '.join(args))
 
+        file_raw = open(file_name + '.pdf');
+
+        if os.path.isfile(file_name + ".pdf"):
+            os.remove(file_name + ".pdf")
+        else:    ## Show an error ##
+            file_name_msg = file_name + ".pdf"
+            print("Error: %s file not found" % file_name_msg)
+
         return Response(
-            wrap_file(request.environ, open(file_name + '.pdf')),
+            wrap_file(request.environ, file_raw),
             mimetype='application/pdf',
         )
 
